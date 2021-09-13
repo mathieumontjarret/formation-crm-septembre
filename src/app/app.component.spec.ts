690ac812e347a6a457bed7c7d311f0957f4334da
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2021-09-13 14:31:19 +0200

    Etape 1: Création des modules

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('application-crm-septembre app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('application-crm-septembre');
   });
 });
